0a5c5ad957ac285a559ef2d469cc923311387dce
umnizy <75345900+umnizy@users.noreply.github.com>
2022-11-11 16:26:38 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+    //show popup
+    $("#show-popup-btn").click(function(){
+        $("#popup-container").show();
+    })
+    //close popup
+    $("#close-btn").click(function(){
+        $("#popup-container").hide();
+    })
+})
